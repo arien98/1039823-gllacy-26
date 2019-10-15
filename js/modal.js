89c6cd6f9eec91cc653c74f5fb8c05cec09fc09b
arien98 <sveta.wing@gmail.com>
2019-10-15 12:01:40 +0500

    Личный проект: оживляем интерактивные элементы

---
+++ js/modal.js
@@ -0,0 +1,58 @@
+var modalLink = document.querySelector(".contacts-button");
+var modalWindow = document.querySelector(".feedback-window");
+var closeLink = document.querySelector(".close-button");
+
+var modalForm = modalWindow.querySelector("form");
+var feedbackName = modalWindow.querySelector("[name=feedback-name]");
+var feedbackEmail = modalWindow.querySelector("[name=feedback-email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("feedbackName");
+} catch (err) {
+  isStorageSupport = false;
+};
+
+modalLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalWindow.classList.add("modal-show");
+
+  if (storage) {
+    feedbackName.value = storage;
+    feedbackEmail.focus();
+  } else {
+    feedbackName.focus();
+  }
+});
+
+closeLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalWindow.classList.remove("modal-show");
+  modalWindow.classList.remove("modal-error");
+});
+
+modalForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!feedbackName.value || !feedbackEmail.value) {
+    evt.preventDefault();
+    modalWindow.classList.remove("modal-error");
+    modalWindow.offsetWidth = modalWindow.offsetWidth;
+    modalWindow.classList.add("modal-error");
+  } else {
+    if (isStorageSupport){
+      localStorage.setItem("feedbackName", feedbackName.value);
+    };
+  }
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (modalWindow.classList.contains("modal-show")) {
+        modalWindow.classList.remove("modal-show");
+        modalWindow.classList.remove("modal-error");
+      }
+    }
+  });
+});
